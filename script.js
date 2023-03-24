56ec54b2bd64bf6b8fc0ba58e0fea5e6382131bb
jaeyun <d65971@gmail.com>
2023-03-24 19:07:46 +1030

    feat: add operate func for basic arithmetic

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+// Takes three parameters: an operator (string) and two numbers (a and b). It performs an arithemtic operation on the two numbers based on the given operator string, and returns the result. Throws an error message if an invalid operator is provided. 
+function operate(operator, a, b) {
+    switch(operator) {
+        case "+":
+            result = a + b;
+            break;
+        case "-":
+            result = a - b;
+            break;
+        case "*":
+            result = a * b;
+            break;
+        case "/":
+            result = a / b;
+            break;
+        default:
+            console.log("Invalid operator");
+      }
+}
